3508c3a20ba82ab809cf40717f2d556c86c4feaa
Kelsey Kosnik <keko9112@colorado.edu>
2021-10-25 23:21:52 -0400

    Adding a footer + Basic table to display dummy data on home page

--- Campr_Flask_Codebase/campr_app.py
+++ Campr_Flask_Codebase/campr_app.py
@@ -2,6 +2,27 @@ from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
+camp_sites = [
+    {
+        'site_name': 'Peaceful Valley',
+        'gps': '40.13196, -105.50705',
+        'city': 'Allenspark',
+        'state_abr': 'CO',
+        'date': '5/15/20',
+        'rating': 4,
+        'notes': 'Nice site, could hear street noise from peak to peak highway though'
+    },
+    {
+        'site_name': 'Meeker Park',
+        'gps': '40.24075, -105.53691',
+        'city': 'Allenspark',
+        'state_abr': 'CO',
+        'date': '9/17/19',
+        'rating': 5,
+        'notes': 'Close to Longs Peak but otherwise not an exciting site'
+    }
+]
+
 @app.route('/')
 @app.route('/login/')
 def login():
@@ -9,7 +30,7 @@ def login():
 
 @app.route('/home/')
 def home():
-    return render_template('home.html', title='Home Page')
+    return render_template('home.html', title='Home Page', camp_sites=camp_sites)
 
 @app.route('/new_user/')
 def new_user():
