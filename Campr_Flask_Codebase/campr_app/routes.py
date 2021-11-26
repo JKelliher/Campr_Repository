0635b0645c9c90ae0eda231351553a1d38f59e1c
Kelsey Kosnik <keko9112@colorado.edu>
2021-11-25 21:01:31 -0500

    Ability to update and delete posts. Create new individual campsite view

--- Campr_Flask_Codebase/campr_app/routes.py
+++ Campr_Flask_Codebase/campr_app/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template, redirect
+from flask import Flask, url_for, render_template, redirect, flash, request
 from campr_app import app, db
 from campr_app.forms import camp_site_entry_form, new_user_form, search_form
 from campr_app.models import CampSites
@@ -38,7 +38,7 @@ def new_campsite():
         db.session.add(campsite)
         db.session.commit()
         return redirect(url_for('home'))
-    return render_template('new_campsite.html', form=form)
+    return render_template('new_campsite.html', form=form, legend='New Post')
 
 
 
@@ -84,3 +84,50 @@ def retrieve_login():
 @app.route('/about/')
 def about():
     return render_template('about.html')
+
+@app.route('/campsite/<int:idcamp>', methods=['GET'])
+def campsite(idcamp):
+    campsite = CampSites.query.get_or_404(idcamp)
+    return render_template('campsite.html', title=campsite.Site_Name, campsite=campsite)
+
+@app.route('/campsite/<int:idcamp>/update', methods=['GET', 'POST'])
+def update_campsite(idcamp):
+    campsite = CampSites.query.get_or_404(idcamp)
+    form = camp_site_entry_form()
+    if form.validate_on_submit():
+        campsite.Site_Name = form.site_name.data
+        campsite.GPS = form.gps_coordinates.data
+        campsite.City = form.city.data
+        campsite.State = form.state.data
+        campsite.Date = form.date_of_visit.data
+        campsite.Rating = form.rating.data
+        campsite.Type = form.type_of_campsite.data
+        campsite.Restrooms = form.restrooms.data
+        campsite.Fees = form.fees.data
+        campsite.Notes = form.notes.data
+        campsite.Image = form.notes.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('campsite', idcamp=campsite.idCamp))
+    elif request.method == 'GET':
+        form.site_name.data = campsite.Site_Name 
+        form.gps_coordinates.data = campsite.GPS
+        form.city.data = campsite.City
+        form.state.data = campsite.State 
+        form.date_of_visit.data = campsite.Date
+        form.rating.data = campsite.Rating 
+        form.type_of_campsite.data = campsite.Type
+        form.restrooms.data = campsite.Restrooms
+        form.fees.data = campsite.Fees
+        form.notes.data = campsite.Notes
+        form.upload_photo.data = campsite.Image
+    return render_template('new_campsite.html', title='Update Post', form=form, legend='Update Post')
+
+
+@app.route('/campsite/<int:camp_id>/delete', methods=['POST'])
+def delete_campsite(camp_id):
+    campsite = CampSites.query.get_or_404(camp_id)
+    db.session.delete(campsite)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
